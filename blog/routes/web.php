cd6264946ec3269e5545001d4776e4d939f0a0ed
someone <someone@someplace.com>
2017-09-25 16:57:01 +0700

    lesson 8

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,19 +11,6 @@
 |
  */
 use App\Task;
-Route::get('/tasks', function () {
-		
-		$tasks = Task::all();
-		return view('tasks.index', compact('tasks'));
-	});
+Route::get('/tasks','TasksController@index');
+Route::get('/tasks/{task}','TasksController@show');
 
-Route::get('tasks/{task}', function ($id) {
-
-		$task = Task::find($id);
-
-		return view('tasks.show', compact('task'));
-	});
-
-Route::get('about', function () {
-		return view('about');
-	});
