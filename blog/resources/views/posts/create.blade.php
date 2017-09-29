ac031fc2b6e187423da4fbad5b62ceb11c6043a1
lam-caotan-mulodo <30790752+lam-caotan-mulodo@users.noreply.github.com>
2017-09-29 14:41:55 +0700

    lesson 12

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -5,13 +5,15 @@
 <form method="post" action="/posts">
 <div class="form-group">
   <label for="ttle">Title</label>
-  <input type="text" class="form-control" id="title">
+  <input type="text" class="form-control" id="title" name="title"  >
 </div>
 <div class="form-group">
   <label for="body">Body</label>
-  <textarea id="body" name="body" class="form-control"></textarea>
+  <textarea id="body" name="body" class="form-control" ></textarea>
 </div>
-<button type="submit" class="btn btn-default">Submit</button>
+<button type="submit" class="btn btn-default">Publish</button>
+@include('layouts.errors')
 </form>
+
 </div>
 @endsection
