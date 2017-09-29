b9689463f6afec0d1416e7bf439f1218a181c59e
lam-caotan-mulodo <30790752+lam-caotan-mulodo@users.noreply.github.com>
2017-09-29 13:44:59 +0700

    lesson 11

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Controllers;
 class PostsController extends Controller
 {
     //
     public function index(){
     	return view('posts.index');
     }
-    public function show(){
-    	return view('posts.show');
+    public function create(){
+      return view('posts.create');
+    }
+    public function store(){
+      dd(Request()->all());
+      Post::create(['title','body']);
+      return redirect('/');
     }
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,5 +2,6 @@
 
 
 Route::get('/','PostsController@index');
-Route::get('/posts/{post}','PostsController@show');
-
+//Route::get('/posts','PostsController@show');
+Route::get('/posts/create','PostsController@create');
+Route::post('/posts','PostsController@store');
